99fad745dd2aa56306095903fa94d780147a8bea
Sara Peksin <peksin.sara@gmail.com>
2021-01-19 12:39:18 +0100

    docker

---
+++ LibraryIS/backend/config/localhost.js
@@ -0,0 +1,36 @@
+const os = require('os');
+
+module.exports = {
+  env: 'localhost',
+
+  database: {
+    username: 'postgres',
+    dialect: 'postgres',
+    password: 'postgres',
+    database: 'Library',
+    host: 'localhost',
+    logging: console.log,
+    operatorsAliases: false,
+  },
+
+
+  email: {
+    comment: 'See https://nodemailer.com',
+    from: '<insert your email here>',
+    host: null,
+    auth: {
+      user: null,
+      pass: null,
+    },
+  },
+
+  graphiql: true,
+
+  clientUrl: 'http://localhost:8080',
+
+  defaultUser: '<insert your email here>',
+
+  uploadDir: os.tmpdir(),
+
+  authJwtSecret: 'af3d8ff4-4076-4641-a08e-d0dc34950674',
+};
---
+++ LibraryIS/backend/config/production.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: 'production',
+
+  database: {
+    username: 'postgres',
+    dialect: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+    host: 'postgres',
+    logging: console.log,
+    operatorsAliases: false,
+  },
+
+
+  email: {
+    comment: 'See https://nodemailer.com',
+    from: '<insert your email here>',
+    host: null,
+    auth: {
+      user: null,
+      pass: null,
+    },
+  },
+
+  graphiql: false,
+
+  clientUrl: 'http://localhost:8080',
+
+  defaultUser: null,
+
+  uploadDir: '/storage',
+
+  authJwtSecret: 'af3d8ff4-4076-4641-a08e-d0dc34950674',
+};
